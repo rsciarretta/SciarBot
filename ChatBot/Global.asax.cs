b3faeaf3073153c9a63db94b33a79197c332e17c
Roberto.Sciarretta <Roberto.Sciarretta@WV-075>
2016-07-12 09:58:14 +0200

    Update to Bot FRamework 3.0.1

--- ChatBot/Global.asax.cs
+++ ChatBot/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace SciarBot
+namespace ChatBot
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
